33183e1a52cac32c48b55f562bd2b3474b47c923
MinhHieu <nguyenminhhieu16012005@gmail.com>
2025-08-17 23:14:16 +0700

    Add Baitapvn.php file

---
+++ Bai1.php
@@ -0,0 +1,8 @@
+<?php
+$chieu_dai = 5;
+$chieu_rong = 3;
+dai * $chieu_rong;
+echo "Chiều dài: $chieu_dai <br>";
+echo "Chiều rộng: $chieu_rong <br>";
+echo "Diện tích hình chữ nhật là: $dien_tich";
+?>
